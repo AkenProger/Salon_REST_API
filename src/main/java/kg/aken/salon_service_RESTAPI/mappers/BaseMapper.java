f84cc7540979f5f1bcb384835fcf52b26182dfa4
AkenProger <meerbek11@mail.ru>
2021-09-02 19:01:47 +0300

    mapper changed

--- src/main/java/kg/aken/salon_service_RESTAPI/mappers/BaseMapper.java
+++ src/main/java/kg/aken/salon_service_RESTAPI/mappers/BaseMapper.java
@@ -1,11 +1,11 @@
 package kg.aken.salon_service_RESTAPI.mappers;
 import java.util.List;
 
-public interface BaseMapper <S, T> {
-    S toDto(T t);
-    T toEntity(S s);
-    List<S> toDtoList(List<T> entities);
-    List<T> toEntityList(List<S> dtoList);
+public interface BaseMapper <DTO, ENTITY> {
+    DTO toDto(ENTITY entity);
+    ENTITY toEntity(DTO dto);
+    List<DTO> toDtoList(List<ENTITY> entities);
+    List<ENTITY> toEntityList(List<DTO> dtoList);
 
 
 }
